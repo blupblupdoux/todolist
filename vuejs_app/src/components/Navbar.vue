3e99d8743e84d9949b2635d22ec2f8ab84c63607
blupblupdoux <sarah.melin19403@gmail.com>
2021-02-01 21:47:12 +0100

    align navbar right

--- vuejs_app/src/components/Navbar.vue
+++ vuejs_app/src/components/Navbar.vue
@@ -1,21 +1,55 @@
 <template>
   <v-app-bar app flat>
+    <div class="navbar d-flex justify-space-between">
       <v-toolbar-title>Todo List</v-toolbar-title>
-      <div class="status">
-        <v-select
-          :items="status"
-          label="Statut"
-        ></v-select>
+      <div class="filter d-flex">
+        <div class="status">
+          <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+              >
+                Statut
+                <v-icon>fa-angle-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in status"
+                :key="index"
+              >
+                <v-list-item-title><a href="#">{{ item }}</a></v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+        <div class="category">
+        <v-menu offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+                v-bind="attrs"
+                v-on="on"
+              >
+                Catégories
+                <v-icon>fa-angle-down</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in categories"
+                :key="index"
+              >
+                <v-list-item-title><a href="#">{{ item }}</a></v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+        <div class="archive">
+          <a href="#">Voir les archives</a>
+        </div>
       </div>
-      <div class="category">
-       <v-select
-          :items="categories"
-          label="Catégories"
-        ></v-select>
-      </div>
-      <div class="archive">
-        <a href="#">Voir les archives</a>
-      </div>
+    </div>
   </v-app-bar>
 </template>
 
@@ -29,3 +63,9 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+  .navbar {
+    width: 100%;
+  }
+</style>
