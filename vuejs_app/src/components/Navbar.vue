13298be191256b8400422bf90ea40794e28371a0
blupblupdoux <sarah.melin19403@gmail.com>
2021-02-01 15:05:20 +0100

    navbar filter layout

--- vuejs_app/src/components/Navbar.vue
+++ vuejs_app/src/components/Navbar.vue
@@ -2,13 +2,19 @@
   <v-app-bar app flat>
       <v-toolbar-title>Todo List</v-toolbar-title>
       <div class="status">
-
+        <v-select
+          :items="status"
+          label="Statut"
+        ></v-select>
       </div>
       <div class="category">
-
+       <v-select
+          :items="categories"
+          label="Catégories"
+        ></v-select>
       </div>
       <div class="archive">
-        
+        <a href="#">Voir les archives</a>
       </div>
   </v-app-bar>
 </template>
@@ -17,5 +23,9 @@
 
 export default {
   name: 'Navbar',
+  data: () => ({
+    status: ['Toutes', 'Complètes', 'Incomplètes'],
+    categories: ['Catego 1', 'Catego 2'],
+  }),
 };
 </script>
