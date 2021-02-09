2f7a0ca4fb057eeae8782cc8bb3f363887f44416
blupblupdoux <sarah.melin19403@gmail.com>
2021-02-09 09:55:31 +0100

    navbar categories from db

--- vuejs_app/src/components/Navbar.vue
+++ vuejs_app/src/components/Navbar.vue
@@ -2,26 +2,27 @@
   <div class="d-flex justify-space-between align-center mx-auto px-10 pt-2  d-flex light-green lighten-4">
     <v-toolbar-title>Todo List</v-toolbar-title>
     <div class="filter d-flex pt-6 ">
-      <div class="filterItem status">
+      <div class="status mr-3">
         <v-select
-        class="pa-0 ma-0"
-        color="light-green darken-4"
-        :items="status"
+          class="pa-0 ma-0"
+          color="light-green darken-4"
+          :items="status"
+          item-text="name"
           label="Statut"
+          clearable
         >
         </v-select>
       </div>
-      <div class="filterItem category">
+      <div class="category mr-3">
         <v-select
           class="pa-0 ma-0"
           color="light-green darken-4"
           :items="categories"
+          item-text="name"
           label="Catégories"
+          clearable
         >
         </v-select>
-      </div>
-      <div class="filterItem archive d-flex align-center">
-        <a  href="#">Voir les archives</a>
       </div>
     </div>
   </div>
@@ -29,26 +30,28 @@
 
 <script>
 
+import { mapState } from 'vuex'
+
 export default {
   name: 'Navbar',
   data: () => ({
-    status: ['Toutes', 'Complètes', 'Incomplètes'],
-    categories: ['Catego 1', 'Catego 2'],
+    status: [{id:"0", name:'Incomplètes'}, {id:"1", name:'Complètes'}, {id:"2", name:'Archivées'}],
+    categories: [],
   }),
+  computed: {
+    ...mapState(['apiURL']),
+  },
+  methods: {
+    getCategories() {
+      this.$axios
+      .get(`${this.apiURL}/category`)
+      .then(response => { this.categories = response.data })
+    }
+  },
+  mounted() {
+    this.getCategories()
+    
+  }
 };
 </script>
 
-<style lang="scss" scoped>
-  .filter {
-
-    .filterItem {
-      margin-right: 20px;
-    }
-
-    .archive > a {
-      white-space: nowrap;
-      color: #33691E;
-    }
-  }
-  
-</style>
